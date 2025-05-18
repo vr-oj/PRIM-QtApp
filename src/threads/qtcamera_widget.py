d6832bc955ce0686e1557b26316d5c70a05e7fe9
vr-oj <lay_genial_07@icloud.com>
2025-05-18 14:18:37 -0400

    Update qtcamera_widget.py

--- src/threads/qtcamera_widget.py
+++ src/threads/qtcamera_widget.py
@@ -1,10 +1,11 @@
 import logging
 
+import imagingcontrol4
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizePolicy
 from PyQt5.QtCore import pyqtSignal, Qt, pyqtSlot
 from PyQt5.QtGui import QImage, QPixmap, QFont
 
-from threads.sdk_camera_thread import SDKCameraThread
+from .sdk_camera_thread import SDKCameraThread
 
 log = logging.getLogger(__name__)
 
@@ -26,7 +27,12 @@ class QtCameraWidget(QWidget):
         self.default_pixel_format = "Mono8"
         self.default_exposure_us = 20000
         self.default_target_fps = 20
-        self.default_gain = 0
+
+        # New defaults for gain/brightness/auto/ROI
+        self.default_gain = None
+        self.default_brightness = None
+        self.default_auto_exposure = False
+        self._default_roi = (0, 0, 0, 0)
 
         self._camera_thread = None
         self._last_pixmap = None
@@ -81,18 +87,18 @@ class QtCameraWidget(QWidget):
             return
 
         self.viewfinder.setText(f"Connecting to {self._active_camera_description}...")
-        thread = SDKCameraThread(
+        t = SDKCameraThread(
             exposure_us=self.default_exposure_us,
             target_fps=self.default_target_fps,
             width=self.default_width,
             height=self.default_height,
             pixel_format=self.default_pixel_format,
         )
-        thread.frame_ready.connect(self._on_sdk_frame_received)
-        thread.camera_error.connect(self._on_camera_thread_error)
-        thread.camera_resolutions_available.connect(self.camera_resolutions_updated)
-        thread.start()
-        self._camera_thread = thread
+        t.frame_ready.connect(self._on_sdk_frame_received)
+        t.camera_error.connect(self._on_camera_thread_error)
+        t.camera_resolutions_available.connect(self.camera_resolutions_updated)
+        t.start()
+        self._camera_thread = t
 
     def set_active_resolution(self, width: int, height: int):
         self.default_width = width
@@ -102,19 +108,77 @@ class QtCameraWidget(QWidget):
                 self._active_camera_id, self._active_camera_description
             )
 
+    @pyqtSlot(int)
     def set_exposure(self, exposure_us: int):
+        """Dynamically change exposure (or store for next start)."""
+        log.info(f"QtCameraWidget: set_exposure({exposure_us})")
+        self.default_exposure_us = exposure_us
         if self._camera_thread and self._camera_thread.isRunning():
-            log.info(f"Queuing exposure change: {exposure_us}")
-            self._camera_thread.update_exposure(exposure_us)
-        else:
-            self.default_exposure_us = exposure_us
+            try:
+                self._camera_thread.pm.set_value(
+                    imagingcontrol4.PropId.EXPOSURE_TIME, exposure_us
+                )
+            except Exception:
+                log.debug("Could not apply exposure on the fly; will apply on restart")
 
+    @pyqtSlot(int)
     def set_gain(self, gain: int):
+        """Dynamically change gain (or store for next start)."""
+        log.info(f"QtCameraWidget: set_gain({gain})")
+        self.default_gain = gain
         if self._camera_thread and self._camera_thread.isRunning():
-            log.info(f"Queuing gain change: {gain}")
-            self._camera_thread.update_gain(gain)
-        else:
-            self.default_gain = gain
+            try:
+                self._camera_thread.pm.set_value(imagingcontrol4.PropId.GAIN, gain)
+            except Exception:
+                log.debug("Could not apply gain on the fly; will apply on restart")
+
+    @pyqtSlot(int)
+    def set_brightness(self, brightness: int):
+        """Dynamically change brightness (or store for next start)."""
+        log.info(f"QtCameraWidget: set_brightness({brightness})")
+        self.default_brightness = brightness
+        if self._camera_thread and self._camera_thread.isRunning():
+            try:
+                self._camera_thread.pm.set_value(
+                    imagingcontrol4.PropId.BRIGHTNESS, brightness
+                )
+            except Exception:
+                log.debug(
+                    "Could not apply brightness on the fly; will apply on restart"
+                )
+
+    @pyqtSlot(bool)
+    def set_auto_exposure(self, enable_auto: bool):
+        """Toggle camera auto-exposure."""
+        log.info(f"QtCameraWidget: set_auto_exposure({enable_auto})")
+        self.default_auto_exposure = enable_auto
+        if self._camera_thread and self._camera_thread.isRunning():
+            try:
+                pid = imagingcontrol4.PropId.AUTO_EXPOSURE
+                self._camera_thread.pm.set_value(pid, "On" if enable_auto else "Off")
+            except Exception:
+                log.debug("Could not toggle auto-exposure on the fly")
+
+    @pyqtSlot(int, int, int, int)
+    def set_software_roi(self, x: int, y: int, w: int, h: int):
+        """Apply a software ROI (or store for next start)."""
+        log.info(f"QtCameraWidget: set_software_roi(x={x}, y={y}, w={w}, h={h})")
+        self._default_roi = (x, y, w, h)
+        if self._camera_thread and self._camera_thread.isRunning():
+            try:
+                self._camera_thread.set_roi(x, y, w, h)
+            except Exception:
+                log.debug("Could not apply ROI on the fly; will apply on restart")
+
+    @pyqtSlot()
+    def reset_roi_to_default(self):
+        """Reset ROI back to full frame."""
+        log.info("QtCameraWidget: reset_roi_to_default()")
+        self.set_software_roi(0, 0, 0, 0)
+        if self._active_camera_id >= 0:
+            self.set_active_camera(
+                self._active_camera_id, self._active_camera_description
+            )
 
     @pyqtSlot(QImage, object)
     def _on_sdk_frame_received(self, qimg: QImage, frame: object):
@@ -128,10 +192,10 @@ class QtCameraWidget(QWidget):
     def _on_camera_thread_error(self, message: str, code: str):
         log.error(f"Camera error: {message} ({code})")
         self.viewfinder.setText(
-            f"Camera Error: {message[:60]}{'...' if len(message)>60 else ''}"
+            f"Camera Error: {message[:60]}{'...' if len(message) > 60 else ''}"
         )
         self.camera_error.emit(message, code)
-        if self._camera_thread and self.sender() == self._camera_thread:
+        if self._camera_thread and self.sender() is self._camera_thread:
             self._camera_thread.stop()
 
     def _update_display(self):
