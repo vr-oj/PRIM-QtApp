6508e1931214e31a39c57229dfd724b05b99aec0
vr-oj <lay_genial_07@icloud.com>
2025-05-19 20:38:59 -0400

    Update main_window.py

--- src/main_window.py
+++ src/main_window.py
@@ -164,7 +164,7 @@ class MainWindow(QMainWindow):
 
         self.main_splitter.addWidget(self.qt_cam_widget)
         self.main_splitter.addWidget(self.pressure_plot_widget)
-        self.main_splitter.setStretchFactor(0, 1)
+        self.main_splitter.setStretchFactor(1, 1)
         self.main_splitter.setStretchFactor(2, 2)
 
         outer_layout.addWidget(self.main_splitter, 1)
