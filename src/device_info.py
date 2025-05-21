524945996c290514436d93a72b3919427b8f6530
vr-oj <lay_genial_07@icloud.com>
2025-05-20 21:11:24 -0400

    Update device_info.py

--- src/device_info.py
+++ src/device_info.py
@@ -3,10 +3,10 @@ import imagingcontrol4 as ic4
 ic4.Library.init()
 
 grabber = ic4.Grabber()
-dev = ic4.DeviceEnum.devices()[0]
-grabber.device_open(dev)
-
+grabber.device_open(ic4.DeviceEnum.devices()[0])
 pm = grabber.device_property_map
-# List any property names containing 'format'
-fmt_props = [prop.name for prop in pm.properties if "format" in prop.name.lower()]
-print("Format-related properties:", fmt_props)
+
+print("=== PropertyMap members ===")
+for attr in sorted(dir(pm)):
+    if not attr.startswith("_"):
+        print(attr)
