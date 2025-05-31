d09cce200334756efa627ca1a505a1bc170265d7
vr-oj <lay_genial_07@icloud.com>
2025-05-31 09:10:41 -0400

    Update camera_view.py

--- liveplotter/canvas/camera_view.py
+++ liveplotter/canvas/camera_view.py
@@ -1,82 +1,35 @@
 # camera_view.py
 
-import numpy as np
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
+import logging
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QSizePolicy
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import Qt, pyqtSlot
 
-try:
-    import imagingcontrol4 as ic4
-
-    IC4_AVAILABLE = True
-except ImportError:
-    IC4_AVAILABLE = False
-    import cv2
+log = logging.getLogger(__name__)
 
 
 class CameraView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.label = QLabel("Camera Feed")
-        self.label.setStyleSheet("background-color: black;")
-        self.label.setScaledContents(True)
 
-        layout = QVBoxLayout()
+        self.label = QLabel("Camera View", self)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setStyleSheet("background-color: black; color: white;")
+        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.label)
         self.setLayout(layout)
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_frame)
-
-        self.ic4_device = None
-        self.opencv_cap = None
-
-    def start_camera(self):
-        if IC4_AVAILABLE:
-            devices = ic4.Device.enumerate()
-            if not devices:
-                print("No IC4 devices found.")
-                return
-            self.ic4_device = devices[0].open()
-            fmt = self.ic4_device.videoFormats()[0]
-            self.ic4_device.setVideoFormat(fmt)
-            self.sink = self.ic4_device.sink()
-            self.stream = self.ic4_device.stream()
-            self.stream.start()
-            print("IC4 camera started.")
-        else:
-            self.opencv_cap = cv2.VideoCapture(0)
-            if not self.opencv_cap.isOpened():
-                print("OpenCV camera not available.")
-                return
-            print("OpenCV camera started.")
-
-        self.timer.start(30)
-
-    def stop_camera(self):
-        self.timer.stop()
-        if IC4_AVAILABLE and self.ic4_device:
-            self.stream.stop()
-            self.ic4_device.close()
-            self.ic4_device = None
-        elif self.opencv_cap:
-            self.opencv_cap.release()
-            self.opencv_cap = None
-
-    def update_frame(self):
-        if IC4_AVAILABLE and self.ic4_device:
-            frame = self.sink.snap()
-            image = QImage(
-                frame.data(), frame.width(), frame.height(), QImage.Format_RGB888
-            )
-        elif self.opencv_cap:
-            ret, frame = self.opencv_cap.read()
-            if not ret:
-                return
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            h, w, ch = frame.shape
-            image = QImage(frame.data, w, h, ch * w, QImage.Format_RGB888)
-        else:
+    @pyqtSlot(QImage)
+    def set_frame(self, image: QImage):
+        if not image or image.isNull():
+            log.warning("Received null image in CameraView.")
             return
-
-        self.label.setPixmap(QPixmap.fromImage(image))
+        pixmap = QPixmap.fromImage(image)
+        self.label.setPixmap(
+            pixmap.scaled(
+                self.label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation
+            )
+        )
