95485919607c570449c85e57c9f6296bc34005e6
vr-oj <lay_genial_07@icloud.com>
2025-06-01 13:15:19 -0400

    Update debug_ic4_api.py

--- prim_app/debug_ic4_api.py
+++ prim_app/debug_ic4_api.py
@@ -1,5 +1,17 @@
-# debug_ic4_api.py
 import imagingcontrol4 as ic4
 
-print("Available attributes in imagingcontrol4:")
-print(dir(ic4))
+print("Library version:", ic4.__version__)
+
+try:
+    ic4.library.Library.init()
+    print("Library initialized.")
+
+    grabber = ic4.grabber.Grabber()
+    print("Grabber initialized.")
+
+    devices = grabber.get_available_video_capture_devices()
+    print("Available devices:")
+    for dev in devices:
+        print("  -", dev.name)
+except Exception as e:
+    print("Error:", e)
