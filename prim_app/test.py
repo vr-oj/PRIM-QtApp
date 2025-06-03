e9e92f4abf73d018f01a95f3c15cdbcf8f15158c
vr-oj <lay_genial_07@icloud.com>
2025-06-03 09:06:21 -0400

    Update test.py

--- prim_app/test.py
+++ prim_app/test.py
@@ -1,46 +1,61 @@
 import imagingcontrol4 as ic4
-import cv2
 
 
-def example_imagebuffer_numpy_opencv_snap():
+def format_device_info(device_info: ic4.DeviceInfo) -> str:
+    return f"Model: {device_info.model_name} Serial: {device_info.serial}"
+
+
+def print_device_list():
+    print("Enumerating all attached video capture devices...")
+
     device_list = ic4.DeviceEnum.devices()
-    for i, dev in enumerate(device_list):
-        print(f"[{i}] {dev.model_name} ({dev.serial}) [{dev.interface.display_name}]")
-    print(f"Select device [0..{len(device_list) - 1}]: ", end="")
-    selected_index = int(input())
-    dev_info = device_list[selected_index]
 
-    grabber = ic4.Grabber()
-    grabber.device_open(dev_info)
+    if len(device_list) == 0:
+        print("No devices found")
+        return
 
-    cv2.namedWindow("display")
+    print(f"Found {len(device_list)} devices:")
 
-    sink = ic4.SnapSink()
-    grabber.stream_setup(sink)
+    for device_info in device_list:
+        print(format_device_info(device_info))
 
-    for i in range(5):
-        print("Focus display window and press any key to continue...")
-        cv2.waitKey(0)
 
-        buffer = sink.snap_single(1000)
-        np = buffer.numpy_wrap()
-        print("Displaying captured image")
-        cv2.imshow("display", np)
+def print_interface_device_tree():
+    print("Enumerating video capture devices by interface...")
 
-        print("Focus display window and press any key to continue...")
-        cv2.waitKey(0)
+    interface_list = ic4.DeviceEnum.interfaces()
 
-        cv2.blur(np, (75, 75), np)
-        print("Displaying blurred image")
-        cv2.imshow("display", np)
+    if len(interface_list) == 0:
+        print("No interfaces found")
+        return
 
-    grabber.stream_stop()
+    for itf in interface_list:
+        print(f"Interface: {itf.display_name}")
+        print(
+            f"\tProvided by {itf.transport_layer_name} [TLType: {str(itf.transport_layer_type)}]"
+        )
+
+        device_list = itf.devices
+
+        if len(device_list) == 0:
+            print("\tNo devices found")
+            continue
+
+        print(f"\tFound {len(device_list)} devices:")
+
+        for device_info in device_list:
+            print(f"\t\t{format_device_info(device_info)}")
+
+
+def example_device_enumeration():
+    print_device_list()
+    print_interface_device_tree()
 
 
 if __name__ == "__main__":
     ic4.Library.init(api_log_level=ic4.LogLevel.INFO, log_targets=ic4.LogTarget.STDERR)
 
     try:
-        example_imagebuffer_numpy_opencv_snap()
+        example_device_enumeration()
     finally:
         ic4.Library.exit()
