4eaad605b5dc8edcdca63e52f63a5a7e83df2caf
vr-oj <lay_genial_07@icloud.com>
2025-06-03 20:04:31 -0400

    yes

--- test scripts/png2ico.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-sizes = [(256, 256), (128, 128), (64, 64), (48, 48), (32, 32), (16, 16)]
-img = Image.open("A_flat_vector_icon_depicts_a_schematic_representat.png")
-img.save("PRIM.ico", sizes=sizes)
