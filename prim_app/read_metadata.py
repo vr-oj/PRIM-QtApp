49aa3483a5d7f674c761e82642c8cf4f5ac055e5
vr-oj <lay_genial_07@icloud.com>
2025-06-06 12:44:49 -0400

    Update read_metadata.py

--- prim_app/read_metadata.py
+++ prim_app/read_metadata.py
@@ -1,7 +1,13 @@
-import tifffile, json
+import tifffile
+import json
 
-with tifffile.TiffFile("recording_XXXX_video.tif") as tif:
+# Replace "recording_video.tif" with the actual file name (or full path)
+tif_path = "recording_2025-06-06_10-09-32_video.tif"
+
+with tifffile.TiffFile(tif_path) as tif:
     for page in tif.pages:
         desc = page.tags["ImageDescription"].value
         data = json.loads(desc)
-        print(data["frameIdx"], data["deviceTime"], data["pressure"])
+        print(
+            f"frameIdx={data['frameIdx']},  deviceTime={data['deviceTime']},  pressure={data['pressure']}"
+        )
