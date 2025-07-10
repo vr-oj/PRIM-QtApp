c570e69b57d79bf87844411c6c514dd5efb64d0c
Valdo Vega Rodríguez <lay_genial_07@icloud.com>
2025-07-10 12:26:34 -0400

    Revert "Add basic Thorlabs camera thread"

--- prim_app/threads/__init__.py
+++ prim_app/threads/__init__.py
@@ -1,11 +1,9 @@
 from .serial_thread import SerialThread
 from .sdk_camera_thread import SDKCameraThread
 from .micromanager_camera_thread import MicroManagerCameraThread
-from .thorlabs_camera_thread import ThorlabsCameraThread
 
 __all__ = [
     "SerialThread",
     "SDKCameraThread",
     "MicroManagerCameraThread",
-    "ThorlabsCameraThread",
 ]
