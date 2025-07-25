6a9c80c1ef95f784c713bddaaaadfa092ee29b9d
vr-oj <lay_genial_07@icloud.com>
2025-07-25 14:27:48 -0400

    Update playback_window.py

--- prim_app/playback_window.py
+++ prim_app/playback_window.py
@@ -184,7 +184,7 @@ class PlaybackWindow(QMainWindow):
         painter = QPainter(qimg)
         painter.setRenderHints(QPainter.Antialiasing | QPainter.TextAntialiasing)
 
-        scale_factor = disp_h / 200
+        scale_factor = disp_h
         font_size = int(self.font_spin.value() * scale_factor)
         font = QFont("Arial", font_size)
         painter.setFont(font)
